45acf2e7e4649cc275f772f87e8a8ea724ba6eef
bobby-rust <bobby.rust121@gmail.com>
2024-07-11 23:42:32 -0400

    refactor: extract navbar buttons into components, fix settings dropdown on very small screens

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -1,12 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
-import { IoSettingsOutline } from "react-icons/io5";
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import BuildGenerator from "@/classes/BuildGenerator";
-import { ItemCategory } from "@/types/enums";
-import { BuildGenerationConfig, defaultBuildGenerationConfig } from "@/types/types";
-import { themes } from "@/types/constants";
+import { defaultBuildGenerationConfig } from "@/types/types";
 import { toast } from "sonner";
+import Settings from "./Settings";
+import BMAC from "./BMAC";
+import Themes from "./Themes";
+
 /**
  * The NavbarMenu component displays a menu with various options for the user.
  * It allows the user to navigate to different pages, change the theme, and change
@@ -19,10 +19,6 @@ import { toast } from "sonner";
  * @returns {JSX.Element} The rendered NavbarMenu component.
  */
 export default function NavbarMenu({ generator, theme, setTheme }: { generator: BuildGenerator; theme: string; setTheme: (theme: string) => void }) {
-	// The current build generation configuration
-	const [config, setConfig] = React.useState<BuildGenerationConfig>(generator._buildGenerationConfig);
-	// The current value of the toggle button
-	const [toggleValue, setToggleValue] = React.useState(true);
 	// The current width of the window
 	const [width, setWidth] = React.useState(window.innerWidth);
 
@@ -30,41 +26,6 @@ export default function NavbarMenu({ generator, theme, setTheme }: { generator: 
 	const navigate = useNavigate();
 	// A regular expression to check if the current URL contains "/ai/"
 	const regexp = new RegExp("/ai/*");
-
-	/**
-	 * Toggles the value of the excludePreviouslyRolled property for all categories
-	 * in the build generation configuration.
-	 *
-	 * @param {React.MouseEvent<HTMLDivElement, MouseEvent>} e - The click event.
-	 */
-	const toggleAll = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-		e.preventDefault();
-		const newConfig: BuildGenerationConfig = { ...config };
-		for (const [c, _] of newConfig.buildInfo.categoryConfigs.entries()) {
-			const currConfig = newConfig.buildInfo.categoryConfigs.get(c as ItemCategory)!;
-			currConfig.excludePreviouslyRolled = toggleValue;
-			!toggleValue && currConfig.previouslyRolled.clear();
-		}
-
-		setToggleValue(!toggleValue);
-		setConfig(newConfig);
-	};
-
-	/**
-	 * Toggles the value of the excludePreviouslyRolled property for a specific category
-	 * in the build generation configuration.
-	 *
-	 * @param {React.MouseEvent<HTMLDivElement, MouseEvent>} e - The click event.
-	 * @param {ItemCategory} c - The category to toggle the excludePreviouslyRolled property for.
-	 */
-	function handleConfigChange(e: React.MouseEvent<HTMLDivElement, MouseEvent>, c: ItemCategory) {
-		e.preventDefault();
-		const newConfig: BuildGenerationConfig = { ...config };
-
-		newConfig.buildInfo.categoryConfigs.get(c as ItemCategory)!.excludePreviouslyRolled = !newConfig.buildInfo.categoryConfigs.get(c as ItemCategory)!.excludePreviouslyRolled;
-		generator.setExcludePreviouslyRolledForCategory(c, newConfig.buildInfo.categoryConfigs.get(c as ItemCategory)!.excludePreviouslyRolled);
-		setConfig(newConfig);
-	}
 
 	const navigateTo = (path: string) => {
 		generator._buildGenerationConfig = structuredClone(defaultBuildGenerationConfig);
@@ -88,11 +49,7 @@ export default function NavbarMenu({ generator, theme, setTheme }: { generator: 
 	return (
 		<div className={`flex flex-col lg:flex-row flex-wrap justify-center items-center gap-4 ${width < 1032 ? "flex-col-reverse" : ""}`}>
 			{/* Render the Buy me a coffee button */}
-			<div className="h-16 flex justify-center items-center w-60 lg:h-16 lg:w-52">
-				<a className="h-full w-full flex justify-center items-center" href="https://www.buymeacoffee.com/bobbyrust" target="_blank" rel="noreferrer">
-					<img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=bobbyrust&button_colour=efb809&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00" />
-				</a>
-			</div>
+			<BMAC />
 
 			{/* Render the Item Dashboard button */}
 			{/* <li className="h-16 flex text-nowrap justify-center items-center">
@@ -115,42 +72,12 @@ export default function NavbarMenu({ generator, theme, setTheme }: { generator: 
 
 			{/* Render the Settings dropdown */}
 			<li className="h-16 flex flex-nowrap justify-center items-center">
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<button className="btn btn-ghost w-60 lg:w-28 tracking-widest flex flex-nowrap">
-							<IoSettingsOutline /> Settings
-						</button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent className="w-56">
-						<DropdownMenuLabel>Exclude Previously Rolled</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						<DropdownMenuCheckboxItem onClick={toggleAll}>Toggle All</DropdownMenuCheckboxItem>
-						{/* Render checkbox items for each category */}
-						{Object.values(ItemCategory).map((c) => (
-							<DropdownMenuCheckboxItem
-								key={c}
-								onClick={(e) => handleConfigChange(e, c as ItemCategory)}
-								checked={config.buildInfo.categoryConfigs.get(c as ItemCategory)!.excludePreviouslyRolled}>
-								{c}
-							</DropdownMenuCheckboxItem>
-						))}
-					</DropdownMenuContent>
-				</DropdownMenu>
+				<Settings generator={generator} />
 			</li>
 
 			{/* Render the theme selection dropdown */}
 			<li className="h-16 flex justify-center items-center w-60 lg:w-28">
-				<select className="select z-50 select-secondary select-bordered w-full" data-choose-theme data-theme value={theme} onChange={(e) => setTheme(e.target.value)}>
-					<option disabled value="Select a theme">
-						Select a theme
-					</option>
-					{/* Render options for each theme */}
-					{themes.map((theme) => (
-						<option key={theme} value={theme}>
-							{theme}
-						</option>
-					))}
-				</select>
+				<Themes theme={theme} setTheme={setTheme} />
 			</li>
 		</div>
 	);
