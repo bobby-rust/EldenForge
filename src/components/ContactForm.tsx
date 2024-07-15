7d68b93a0a9c235b9d41a7448bc8fb1c78d11d23
bobby-rust <bobby.rust121@gmail.com>
2024-07-15 14:17:01 -0400

    style: add padding to contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 export default function ContactForm() {
 	return (
-		<div className="flex justify-center items-center">
+		<div className="flex justify-center items-center p-5">
 			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKjXTfGT-V301HBSBo-3lXc5UCGGKbq9TfJWEbwLJBePcGoA/viewform?embedded=true" width={640} height={1087} className="frame">
 				Loading…
 			</iframe>
